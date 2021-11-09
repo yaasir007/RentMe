78324a5c9d1351a00a1a6bc48655cbd0f3269db6
mohammad yaasir cheekoory <yaasir1997@gmail.com>
2021-11-09 16:56:02 +0400

    Creating models - review, booking and created controller - booking review cars

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,18 @@
 Rails.application.routes.draw do
   devise_for :users
-  root to: 'pages#home'
-  resources :cars
+  
+  resources :cars do
+    resources :bookings
+  end
+
+  devise_scope :user do
+    authenticated :user do
+      root "cars#index", as: :authenticated_root
+    end
+    unauthenticated do
+      root "pages#home", as: :unauthenticated_root
+    end
+  end
+  # root to: "pages#home"
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
