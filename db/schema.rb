07b5bf09e86e28591e28b2aa861a66ddd1e97631
sharmagowreea <agowreea@hotmail.com>
2021-11-09 11:35:09 +0400

    navbar

--- db/schema.rb
+++ db/schema.rb
@@ -10,8 +10,8 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
+ActiveRecord::Schema.define(version: 2021_11_08_132854) do
 
-ActiveRecord::Schema.define(version: 2021_11_08_115902) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -28,4 +28,5 @@ ActiveRecord::Schema.define(version: 2021_11_08_115902) do
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
+
 end
