ab6f08c47f8f4f4581ce5c34e0d790669ff15163
mohammad yaasir cheekoory <yaasir1997@gmail.com>
2021-11-09 11:29:44 +0400

    Car Seed Completed with 10 cars

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,16 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+require 'faker'
+
+puts "Creating 10 cars with make, details"
+
+10.times do 
+    cars = Car.new(
+        make: Faker::Vehicle.make,
+        details: "Model: #{Faker::Vehicle.make_and_model},Color: #{Faker::Vehicle.color},Transmission: #{Faker::Vehicle.transmission},Drive Type: #{Faker::Vehicle.drive_type}",
+        user_id: 1
+    )
+    cars.save!
+end
+puts "Seeds Completed"
