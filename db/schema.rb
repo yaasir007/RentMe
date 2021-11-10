b3243b7516f2f53cdd947202899cff8df2bf8b23
mohammad yaasir cheekoory <yaasir1997@gmail.com>
2021-11-10 13:18:54 +0400

    cancel booking and created review

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_11_09_142630) do
+ActiveRecord::Schema.define(version: 2021_11_10_063630) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -22,6 +22,7 @@ ActiveRecord::Schema.define(version: 2021_11_09_142630) do
     t.datetime "updated_at", precision: 6, null: false
     t.date "start_date"
     t.date "end_date"
+    t.text "comment"
     t.index ["car_id"], name: "index_bookings_on_car_id"
     t.index ["user_id"], name: "index_bookings_on_user_id"
   end
