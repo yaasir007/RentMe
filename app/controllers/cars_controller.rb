6388a5e6e0794acc4de36ea08272e19914463646
mohammad yaasir cheekoory <yaasir1997@gmail.com>
2021-11-11 13:00:28 +0400

    Btns removed and add review and booking on car/show

--- app/controllers/cars_controller.rb
+++ app/controllers/cars_controller.rb
@@ -7,5 +7,8 @@ class CarsController < ApplicationController
     def show
         @user = current_user
         @car = Car.find(params[:id])
+        @booking = Booking.new
+        @reviews = Review.all
+        @review = Review.new
     end
 end
